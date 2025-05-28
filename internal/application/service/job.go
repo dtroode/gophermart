8517dead8ced8005434b4d65d89ca1075723c9ed
David Kistauri <davidkis113@gmail.com>
2025-05-28 22:18:07 +0300

    refactor: flatten job

--- internal/application/service/job.go
+++ internal/application/service/job.go
@@ -12,6 +12,49 @@ import (
 	"github.com/google/uuid"
 )
 
+func (s *Service) isRetryableError(err error) bool {
+	return errors.Is(err, application.ErrAccrualTooManyRequests) || errors.Is(err, application.ErrAccrualOrderNotRegistered)
+}
+
+func (s *Service) updateOrderStatus(ctx context.Context, id uuid.UUID, status model.OrderStatus) (*model.Order, error) {
+	params := &storage.SetOrderStatus{
+		ID:     id,
+		Status: status,
+	}
+	order, err := s.storage.SetOrderStatus(ctx, params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update order in storage: %w", err)
+	}
+
+	return order, nil
+}
+
+func (s *Service) finalizeInvalidOrder(ctx context.Context, id uuid.UUID) (*model.Order, error) {
+	params := &storage.SetOrderStatus{
+		ID:     id,
+		Status: model.OrderStatusInvalid,
+	}
+	updatedOrder, err := s.storage.SetOrderStatus(ctx, params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update order in storage: %w", err)
+	}
+
+	return updatedOrder, nil
+}
+
+func (s *Service) finalizeProcessedOrder(ctx context.Context, orderID uuid.UUID, accrual float32) (any, error) {
+	params := &storage.SetOrderStatusAndAccrual{
+		ID:      orderID,
+		Status:  model.OrderStatusProcessed,
+		Accrual: int32(accrual * 100.0),
+	}
+	order, err := s.storage.SetOrderStatusAndAccrual(ctx, params)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update order in storage: %w", err)
+	}
+	return order, nil
+}
+
 func (s *Service) checkOrderJob(orderID uuid.UUID, orderNumber string, duration time.Duration) func(ctx context.Context) (any, error) {
 	return func(ctx context.Context) (any, error) {
 		c := time.Tick(duration)
@@ -21,57 +64,28 @@ func (s *Service) checkOrderJob(orderID uuid.UUID, orderNumber string, duration 
 			case <-c:
 				order, err := s.accrualAdapter.GetOrder(ctx, orderNumber)
 				if err != nil {
-					if errors.Is(err, application.ErrAccrualTooManyRequests) {
-						continue
-					}
-					if errors.Is(err, application.ErrAccrualOrderNotRegistered) {
+					if s.isRetryableError(err) {
 						continue
 					}
 
 					return nil, fmt.Errorf("failed to check order: %w", err)
 				}
 
-				if order.Status == model.AccrualOrderStatusProcessing {
+				switch order.Status {
+				case model.AccrualOrderStatusProcessing:
 					if currentStatus != order.Status {
-						params := &storage.SetOrderStatus{
-							ID:     orderID,
-							Status: model.OrderStatusProcessing,
-						}
-						_, err := s.storage.SetOrderStatus(ctx, params)
-						if err != nil {
-							return nil, fmt.Errorf("failed to update order in storage: %w", err)
+						if _, err := s.updateOrderStatus(ctx, orderID, model.OrderStatusProcessing); err != nil {
+							return nil, err
 						}
 						currentStatus = order.Status
 					}
-
 					continue
-				}
-
-				if order.Status == model.AccrualOrderStatusInvalid {
-					params := &storage.SetOrderStatus{
-						ID:     orderID,
-						Status: model.OrderStatusInvalid,
-					}
-					updatedOrder, err := s.storage.SetOrderStatus(ctx, params)
-					if err != nil {
-						return nil, fmt.Errorf("failed to update order in storage: %w", err)
-					}
-
-					return updatedOrder, nil
-				}
-
-				if order.Status == model.AccrualOrderStatusProcessed {
-					params := &storage.SetOrderStatusAndAccrual{
-						ID:      orderID,
-						Status:  model.OrderStatusProcessed,
-						Accrual: int32(order.Accrual * 100.0),
-					}
-					updatedOrder, err := s.storage.SetOrderStatusAndAccrual(ctx, params)
-					if err != nil {
-						return nil, fmt.Errorf("failed to update order in storage: %w", err)
-					}
-
-					return updatedOrder, nil
+				case model.AccrualOrderStatusInvalid:
+					return s.finalizeInvalidOrder(ctx, orderID)
+				case model.AccrualOrderStatusProcessed:
+					return s.finalizeProcessedOrder(ctx, orderID, order.Accrual)
+				default:
+					continue
 				}
 			case <-ctx.Done():
 				if err := ctx.Err(); err != nil {
